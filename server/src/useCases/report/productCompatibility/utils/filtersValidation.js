ea8ebe2c5bcb7c3bca13085bcf19ecdb5f997154
mgrochalskisoldevelo <mgrochalski@soldevelo.com>
2023-03-03 13:37:15 +0100

    TECH-408: Added log about exceeded limit parameter in the request

--- server/src/useCases/report/productCompatibility/utils/filtersValidation.js
+++ server/src/useCases/report/productCompatibility/utils/filtersValidation.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 module.exports = class FiltersValidation {
   static MAX_REPORTS_IN_REQUEST = 1000;
 
@@ -6,6 +7,10 @@ module.exports = class FiltersValidation {
   }
 
   static maxLimitCheck(limit) {
-    return limit > this.MAX_REPORTS_IN_REQUEST ? this.MAX_REPORTS_IN_REQUEST : limit;
+    if (limit > this.MAX_REPORTS_IN_REQUEST) {
+      console.log(`Provided limit parameter: ${limit} exceeded maximum limit: ${this.MAX_REPORTS_IN_REQUEST} and has been set to: ${this.MAX_REPORTS_IN_REQUEST}`);
+      return this.MAX_REPORTS_IN_REQUEST;
+    }
+    return limit;
   }
 };
