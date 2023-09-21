b14dc3062d06ba0dc4cfae5b675e89c6d7cb2987
Paweł Gesek <pgesek@soldevelo.com>
2023-09-21 17:06:28 +0200

    Prepare docker config for staging server

--- deployment/run-prod.sh
+++ deployment/run-prod.sh
@@ -4,9 +4,14 @@ set -e
 
 cd "$(dirname "$0")"
 
+env=$1
+
+# TODO: pass env to this script based on circle branch
+# TODO: check if $env defined and correct
+
 # Do not use the override compose file, which is intended for development
 # We run in prod mode, but we still build from this guild repository
-docker-compose -f ../docker-compose.yml -f ../docker-compose.prod.yml up --build -d --force-recreate
+docker-compose -f ../docker-compose.yml -f "../docker-compose.${env}.yml" up --build -d --force-recreate
 
 # Clean up dangling images
 docker image prune -f
