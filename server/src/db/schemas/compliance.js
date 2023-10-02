6f69c7e410e79b5ddc23b3bedb5c6c8e6c71172d
lruzicki <lukasz.ruz@gmail.com>
2023-10-02 15:52:55 +0200

    TECH-894: compliance schema extended with enum numbers

--- server/src/db/schemas/compliance.js
+++ server/src/db/schemas/compliance.js
@@ -1,88 +1,98 @@
 const mongoose = require('mongoose');
 
+const StatusEnum = {
+  DRAFT: 0,
+  IN_REVIEW: 1,
+  APPROVED: 2,
+  REJECTED: 3
+};
+
+const SpecificationComplianceLevel = {
+  NA: -1,
+  LEVEL_1: 1,
+  LEVEL_2: 2
+};
+
 // SCHEMA FORM CONTENT
 const ComplianceDetailSchema = new mongoose.Schema({
-    bbSpecification: {
-        type: String,
-        required: true
+  bbSpecification: {
+    type: String,
+    required: true
+  },
+  bbVersion: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: Number,
+    enum: [StatusEnum.DRAFT, StatusEnum.IN_REVIEW, StatusEnum.APPROVED, StatusEnum.REJECTED],
+    default: StatusEnum.DRAFT
+  },
+  submissionDate: {
+    type: Date,
+    default: Date.now
+  },
+  deploymentCompliance: {
+    isCompliant: {
+      type: Boolean,
+      required: true
     },
-    bbVersion: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        enum: ['in review', 'approved', 'rejected'],
-        default: 'in review'
-    },
-    submissionDate: {
-        type: Date,
-        default: Date.now
-    },
-    deploymentCompliance: {
-        isCompliant: {
-            type: Boolean,
-            required: true
-        },
-        details: {
-            type: String
-        }
-    },
-    requirementSpecificationCompliance: {
-        level: {
-            type: Number,
-            enum: [-1, 1, 2]
-        },
-        note: String
-    },
-    interfaceCompliance: {
-        level: {
-            type: Number,
-            enum: [-1, 1, 2]
-        },
-        note: String
+    details: {
+      type: String
     }
+  },
+  requirementSpecificationCompliance: {
+    level: {
+      type: Number,
+      enum: Object.values(SpecificationComplianceLevel),
+      required: true
+    }
+  },
+  interfaceCompliance: {
+    level: {
+      type: Number,
+      enum: Object.values(SpecificationComplianceLevel),
+      required: true
+    }
+  }
 });
-  
-  const ComplianceVersionSchema = new mongoose.Schema({
-    version: {
-      type: String,
-      required: true
-    },
-    bbDetails: {
-      type: Map,
-      of: ComplianceDetailSchema,
-      required: true
-    }
-  });
-  
-  const ComplianceReportSchema = new mongoose.Schema({
-    softwareName: {
-      type: String,
-      required: true
-    },
-    logo: {
-      type: String,
-      required: true
-    },
-    website: {
-      type: String,
-      required: true,
-      match: /^http[s]?:\/\/.*/
-    },
-    documentation: [{
-      type: String,
-      required: true,
-      match: /^http[s]?:\/\/.*/
-    }],
-    pointOfContact: {
-      type: String,
-      required: true
-    },
-    compliance: [ComplianceVersionSchema]
-  });
-  
-  const ComplianceReport = mongoose.model('ComplianceReport', ComplianceReportSchema);
-  
-  module.exports = ComplianceReport;
-  
+
+const ComplianceVersionSchema = new mongoose.Schema({
+  version: {
+    type: String,
+    required: true
+  },
+  bbDetails: {
+    type: Map,
+    of: ComplianceDetailSchema,
+    required: true
+  }
+});
+
+const ComplianceReportSchema = new mongoose.Schema({
+  softwareName: {
+    type: String,
+    required: true
+  },
+  logo: {
+    type: String,
+    required: true
+  },
+  website: {
+    type: String,
+    required: true,
+  },
+  documentation: [{
+    type: String,
+    required: true,
+  }],
+  pointOfContact: {
+    type: String,
+    required: true
+  },
+  compliance: [ComplianceVersionSchema]
+});
+
+const ComplianceReport = mongoose.model('ComplianceReport', ComplianceReportSchema);
+
+module.exports = ComplianceReport;
