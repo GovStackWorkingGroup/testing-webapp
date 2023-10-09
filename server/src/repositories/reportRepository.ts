6dfed8332a694e637f72158b3a87cf9547cba3aa
lruzicki <lukasz.ruz@gmail.com>
2023-10-09 19:04:48 +0200

    added types for reportRepository

--- server/src/repositories/reportRepository.ts
+++ server/src/repositories/reportRepository.ts
@@ -1,18 +1,29 @@
-const reportRepository = (repository) => {
-  const add = (report, callback) => repository.add(report, callback);
-  function aggregateCompatibilityByProduct(filters, sorting, callback) {
+import { Callback } from "mongoose";
+import { Filters } from "../../@types/shared/commonTypes";
+
+interface ReportRepository {
+  add(report: Document, callback: Callback): void;
+  aggregateCompatibilityByProduct(filters: Filters, sorting: Record<string, number>, callback: Callback): void;
+  productsCount(filters: Filters, callback: Callback): void;
+  aggregateBBDetailsByProductId(id: String, sorting: Record<string, number>, callback: Callback): void;
+  aggregateByBuildingBlock(callback: Callback): void;
+}
+
+const reportRepository = (repository: ReportRepository): ReportRepository => {
+  const add = (report: Document, callback: Callback) =>repository.add(report, callback);
+  function aggregateCompatibilityByProduct(filters: Filters, sorting: Record<string, number>, callback: Callback) {
     return repository.aggregateCompatibilityByProduct(filters, sorting, callback);
   }
 
-  function productsCount(filters, callback) {
+  function productsCount(filters: Filters, callback: Callback) {
     return repository.productsCount(filters, callback);
   }
 
-  function aggregateBBDetailsByProductId(id, sorting, callback) {
+  function aggregateBBDetailsByProductId(id: String, sorting: Record<string, number>, callback: Callback) {
     return repository.aggregateBBDetailsByProductId(id, sorting, callback);
   }
 
-  function aggregateByBuildingBlock(callback) {
+  function aggregateByBuildingBlock(callback: Callback) {
     return repository.aggregateByBuildingBlock(callback);
   }
 
