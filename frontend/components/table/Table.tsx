7fd03a49acfe6c6acfa751255339256a380a74ea
Karolina Kopacz <karolina.kopacz94@gmail.com>
2023-10-26 11:52:30 +0200

    TECH-865: Render data in the table, added service

--- frontend/components/table/Table.tsx
+++ frontend/components/table/Table.tsx
@@ -1,114 +1,176 @@
+import Link from 'next/link';
 import { RiArrowRightSLine, RiCheckFill } from 'react-icons/ri';
-type CellValue = {
-  value: string;
-};
+import { Cell, DataRow } from '../../service/types';
 
-type Cell = CellValue | { values: CellValue[] };
-
-type DataRow = {
-  cell: Cell[];
-  subHeader?: string;
-};
 type TableProps = {
-  data: { headers: string[]; rows: DataRow[] };
+  data: { headers: string[]; rows: DataRow[] } | Record<string, never>;
   hasVerticalBorders?: boolean;
-  handleRedirect?: (value: string | undefined) => void;
 };
 
-const Table = ({
-  data,
-  hasVerticalBorders = true,
-  handleRedirect,
-}: TableProps) => {
+const Table = ({ data, hasVerticalBorders = true }: TableProps) => {
+  const formatDateIfDate = (value: Cell) => {
+    if (typeof value === 'string') {
+      const date = new Date(value);
+      if (!isNaN(date.getTime())) {
+        return date.toLocaleDateString();
+      } else {
+        return value;
+      }
+    } else {
+      return value;
+    }
+  };
+
   return (
-    <table className="main-table">
-      <thead>
-        <tr>
-          {data.headers.map((header, indexKey) => (
-            <th
-              key={`header-${header}-${indexKey}`}
-              className={`${hasVerticalBorders ? '' : 'no-vertical-border'}`}
-            >
-              {header}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.rows.map((row, indexKey) => (
-          <>
-            {row.subHeader && handleRedirect && (
-              <tr
-                key={`subheader-${row.subHeader}-${indexKey}`}
-                className="tr-subheader"
-              >
-                <td colSpan={5}>
-                  <div
-                    className="tr-subheader-container"
-                    onClick={() => handleRedirect(row.subHeader)}
-                  >
-                    <p>{row.subHeader}</p>
-                    <div className="test-details-arrow">
-                      <RiArrowRightSLine />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            )}
-            <tr key={`row-${row}-${indexKey}`}>
-              {row.cell.map((cell, indexKey) => {
-                if ('value' in cell) {
-                  if (cell.value === 'checked') {
-                    return (
-                      <td
-                        key={`details-cell-check-${cell.value}-${indexKey}`}
-                        className={`${
-                          hasVerticalBorders ? '' : 'no-vertical-border'
-                        }`}
-                      >
-                        <RiCheckFill className="check-icon" />
-                      </td>
-                    );
-                  }
-
-                  return (
-                    <td
-                      key={`details-cell-${cell.value}-${indexKey}`}
-                      className={`${
-                        hasVerticalBorders ? '' : 'no-vertical-border'
-                      }`}
-                    >
-                      {cell.value}
-                    </td>
-                  );
-                }
-
-                if ('values' in cell) {
-                  return (
-                    <td
-                      className="td-row-details"
-                      key={`divided-row-${cell}-${indexKey}`}
-                    >
-                      <table className="inside-table">
-                        <tbody>
-                          {cell.values.map((value) => (
-                            <tr
-                              key={`details-divided-cell-${value}-${indexKey}`}
-                            >
-                              <td>{value.value}</td>
-                            </tr>
-                          ))}
-                        </tbody>
-                      </table>
-                    </td>
-                  );
-                }
-              })}
+    <>
+      {data && (
+        <table className="main-table">
+          <thead>
+            <tr>
+              {data.headers?.map((header, indexKey) => (
+                <th
+                  key={`header-${header}-${indexKey}`}
+                  className={`${
+                    hasVerticalBorders ? '' : 'no-vertical-border'
+                  }`}
+                >
+                  {header}
+                </th>
+              ))}
             </tr>
-          </>
-        ))}
-      </tbody>
-    </table>
+          </thead>
+          <tbody>
+            {data.rows?.map((row, indexKey) => (
+              <>
+                {row.subHeader && (
+                  <tr
+                    key={`subheader-${row.subHeader}-${indexKey}`}
+                    className="tr-subheader"
+                  >
+                    <td colSpan={8}>
+                      <Link
+                        className="tr-subheader-container"
+                        href={{
+                          pathname: `softwareComplianceTesting/details/${row.subHeader}`,
+                        }}
+                      >
+                        <p>{row.subHeader}</p>
+                        <div className="test-details-arrow">
+                          <RiArrowRightSLine />
+                        </div>
+                      </Link>
+                    </td>
+                  </tr>
+                )}
+                <tr key={`row-${row}-${indexKey}`}>
+                  {row.cell.map((cell, indexKey) => {
+                    if ('value' in cell) {
+                      if (cell.value === 'checked') {
+                        return (
+                          <td
+                            key={`details-cell-check-${cell.value}-${indexKey}`}
+                            className={`${
+                              hasVerticalBorders ? '' : 'no-vertical-border'
+                            }`}
+                          >
+                            <RiCheckFill className="check-icon" />
+                          </td>
+                        );
+                      }
+
+                      if (
+                        [
+                          'In Review',
+                          'Rejected',
+                          'Approved',
+                          -1,
+                          1,
+                          2,
+                          true,
+                        ].includes(cell.value)
+                      ) {
+                        return (
+                          <td
+                            key={`details-cell-check-${cell.value}-${indexKey}`}
+                            className={`${
+                              hasVerticalBorders ? '' : 'no-vertical-border'
+                            }`}
+                          >
+                            {cell.value === 'In Review' && (
+                              <p className="td-text-color-container status-in-review">
+                                {cell.value}
+                              </p>
+                            )}
+                            {cell.value === 'Rejected' && (
+                              <p className="td-text-color-container status-rejected">
+                                {cell.value}
+                              </p>
+                            )}
+                            {cell.value === 'Approved' && (
+                              <p className="td-text-color-container status-approved">
+                                {cell.value}
+                              </p>
+                            )}
+                            {cell.value === -1 && (
+                              <p className="td-text-color-container status-na">
+                                N/A
+                              </p>
+                            )}
+                            {cell.value === 1 && (
+                              <p className="td-text-color-container status-level-one">
+                                Level 1
+                              </p>
+                            )}
+                            {cell.value === 2 && (
+                              <p className="td-text-color-container status-level-two">
+                                Level 2
+                              </p>
+                            )}
+                            {cell.value === true && <RiCheckFill />}
+                          </td>
+                        );
+                      }
+
+                      return (
+                        <td
+                          key={`details-cell-${cell.value}-${indexKey}`}
+                          className={`${
+                            hasVerticalBorders ? '' : 'no-vertical-border'
+                          }`}
+                        >
+                          {formatDateIfDate(cell.value)}
+                        </td>
+                      );
+                    }
+
+                    if ('values' in cell) {
+                      return (
+                        <td
+                          className="td-row-details"
+                          key={`divided-row-${cell}-${indexKey}`}
+                        >
+                          <table className="inside-table">
+                            <tbody>
+                              {cell.values.map((value) => (
+                                <tr
+                                  key={`details-divided-cell-${value}-${indexKey}`}
+                                >
+                                  <td>{value.value}</td>
+                                </tr>
+                              ))}
+                            </tbody>
+                          </table>
+                        </td>
+                      );
+                    }
+                  })}
+                </tr>
+              </>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 };
 
