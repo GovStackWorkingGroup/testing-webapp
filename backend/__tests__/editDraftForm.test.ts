f7e223ab167df05585d844728466b4227352b31f
lruzicki <lukasz.ruz@gmail.com>
2023-11-07 13:00:56 +0100

    TECH-880: added test for invalid status

--- backend/__tests__/editDraftForm.test.ts
+++ backend/__tests__/editDraftForm.test.ts
@@ -4,7 +4,8 @@ import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import app from '../server';
 import ComplianceReport from '../src/db/schemas/compliance';
-import complianceData from './testData/complianceTestData.json'; 
+import complianceData from './testData/complianceTestData.json';
+import { StatusEnum } from 'myTypes';
 
 describe('Compliance API', () => {
   let mongod;
@@ -18,8 +19,8 @@ describe('Compliance API', () => {
       await mongoose.disconnect();
     }
     await mongoose.connect(uri);
-    const createdCompliance = await ComplianceReport.create(complianceData);
-      
+    const createdCompliance = await ComplianceReport.create(complianceData[0]);
+
   });
 
   after(async () => {
@@ -29,26 +30,47 @@ describe('Compliance API', () => {
   });
 
   describe('Compliance Report Draft Update', () => {
-  it('should return status 200 and the data exists', async () => {
-    const res = await request(app).get('/compliance/list');
-    expect(res.statusCode).to.equal(200);
-    expect(res.body).to.not.be.empty;
+    it('should return status 200 and the data exists', async () => {
+      const res = await request(app).get('/compliance/list');
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.not.be.empty;
+    });
+
+    it('should return an error when trying to update a non-existent report', async () => {
+      const nonExistentUniqueId = '34e7dfbf-5de0-4146-8946-321378713533';
+      const updateData = {
+        softwareName: "Updated openIMIS",
+      };
+
+      const res = await request(app).patch(`/compliance/drafts/${nonExistentUniqueId}`).send(updateData);
+      // expect(res.statusCode).to.equal(404); // Or another appropriate error code
+      expect(res.body).to.have.property('success', false);
+      expect(res.body).to.have.property('error', 'Draft with unique ID 34e7dfbf-5de0-4146-8946-321378713533 does not exist.');
+    });
+
   });
 
-  it('should return an error when trying to update a non-existent report', async () => {
-    const nonExistentUniqueId = '34e7dfbf-5de0-4146-8946-321378713533';
-    const updateData = {
-      softwareName: "Updated openIMIS",
-    };
-
-    const res = await request(app).patch(`/compliance/drafts/${nonExistentUniqueId}`).send(updateData);
-    console.log(res);
-    console.log(res.body);
-    // expect(res.statusCode).to.equal(404); // Or another appropriate error code
-    expect(res.body).to.have.property('success', false);
-    expect(res.body).to.have.property('error', 'Draft with unique ID 34e7dfbf-5de0-4146-8946-321378713533 does not exist.');
-  });
+  describe('Compliance Report Draft Update with status not equal to draft', () => {
+    const statuses = [
+      { status: StatusEnum.IN_REVIEW, name: "Original Software Name" },
+      { status: StatusEnum.APPROVED, name: "Original Software Name APPROVED" },
+      { status: StatusEnum.REJECTED, name: "Original Software Name REJECTED" },
+    ];
   
-});
-
+    statuses.forEach(({ status, name }) => {
+      it(`should not allow updating softwareName when status is ${status}`, async () => {
+        let draftData = { ...complianceData[2], softwareName: name, status: status };
+        let draft = await ComplianceReport.create(draftData);
+        const updateData = { softwareName: "Updated Software Name" };
+        const res = await request(app).patch(`/compliance/drafts/${draft.uniqueId}`).send(updateData);
+  
+        expect(res.statusCode).not.to.equal(200);
+        expect(res.body).to.have.property('success', false);
+        expect(res.body).to.have.property('error', 'You cannot edit a form that is not in the draft status.');
+  
+        const updatedDraft = await ComplianceReport.findOne({ softwareName: draft.softwareName });
+        expect(updatedDraft?.softwareName).to.equal(name);
+      });
+    });
+  });
 });
