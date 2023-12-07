b72be8cc6aadc6b1a1424e2d1485a48105714b13
lruzicki <lukasz.ruz@gmail.com>
2023-12-07 13:38:00 +0100

    TECH-990: added GitHub auth

--- backend/src/useCases/handleGitHubLogin.ts
+++ backend/src/useCases/handleGitHubLogin.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
-import axios from 'axios';
-import { appConfig } from '../config'; // Konfiguracja aplikacji
+import axios, { AxiosError } from 'axios';
+import { appConfig } from '../config';
 
 export default class GitHubLoginHandler {
 
@@ -16,7 +16,6 @@ export default class GitHubLoginHandler {
     const clientSecret = appConfig.gitHub.clientSecret;
 
     try {
-      // change code for access token
       const githubResponse = await axios.post('https://github.com/login/oauth/access_token', {
         client_id: clientId,
         client_secret: clientSecret,
@@ -28,11 +27,21 @@ export default class GitHubLoginHandler {
       });
 
       const accessToken = githubResponse.data.access_token;
-
       res.redirect(`http://localhost:3000?token=${accessToken}`);
     } catch (error) {
-      console.error('Error during GitHub Callback:', error);
-      res.status(500).send('Authentication failed');
+      if (axios.isAxiosError(error)) {
+        console.error('Axios error during GitHub Callback:', error.message);
+        if (error.response) {
+          res.status(error.response.status).send(error.response.data);
+        } else if (error.request) {
+          res.status(500).send('No response received from GitHub');
+        } else {
+          res.status(500).send('Error in setting up the request');
+        }
+      } else {
+        console.error('Non-Axios error during GitHub Callback:', error);
+        res.status(500).send('Authentication failed due to server error');
+      }
     }
   }
 }
