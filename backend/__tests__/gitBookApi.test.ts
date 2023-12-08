84b1d05508610a31f74a841a80130e7be50640bc
lruzicki <lukasz.ruz@gmail.com>
2023-12-08 15:10:29 +0100

    TECH-986: added tests for cross-cutting requirements

--- backend/__tests__/gitBookApi.test.ts
+++ backend/__tests__/gitBookApi.test.ts
@@ -55,7 +55,7 @@ describe('GitBook API', () => {
 
         });
 
-        it('should successfully create bb requirements record', async () => {
+        it('should successfully create bb requirements record with functional requirements', async () => {
             // Read test data from JSON file
             const testDataPath = path.join(__dirname, 'testData', 'gitBookDocumentFunctionalRequirements.json');
             const testData = JSON.parse(fs.readFileSync(testDataPath, 'utf8'));
@@ -98,4 +98,76 @@ describe('GitBook API', () => {
           });
     });
 
+    describe('GitBook - Cross-Cutting Requirements', () => {
+        it('should successfully process the data from the file and save it', async () => {
+            // Read test data from JSON file
+            const testDataPath = path.join(__dirname, 'testData', 'gitBookDocumentCrossCuttingRequirements.json');
+            const testData = JSON.parse(fs.readFileSync(testDataPath, 'utf8'));
+
+            // Process the data
+            const contentManager = new GitBookPageContentManager();
+            const result = contentManager.extractCrossCuttingRequirements(testData);
+            if ('error' in result) {
+                throw result.error;
+            }
+            const { requirements } = result;
+
+            console.log("--------");
+            console.log(requirements);
+            console.log("--------");
+
+            expect(requirements).to.be.an('array').with.lengthOf(3);
+            expect(requirements[0]).to.have.property('status', RequirementStatusEnum.REQUIRED);
+            expect(requirements[0]).to.have.property('requirement');
+
+            expect(requirements[1]).to.have.property('status', RequirementStatusEnum.RECOMMENDED);
+            expect(requirements[1]).to.have.property('requirement');
+
+            expect(requirements[2]).to.have.property('status', RequirementStatusEnum.OPTIONAL);
+            expect(requirements[2]).to.have.property('requirement');
+
+        });
+
+        it('should successfully create bb requirements record with cross-cutting requirements', async () => {
+            // Read test data from JSON file
+            const testDataPath = path.join(__dirname, 'testData', 'gitBookDocumentCrossCuttingRequirements.json');
+            const testData = JSON.parse(fs.readFileSync(testDataPath, 'utf8'));
+          
+            // Process the data
+            const contentManager = new GitBookPageContentManager();
+            const result = contentManager.extractCrossCuttingRequirements(testData);
+            if ('error' in result) {
+                throw result.error;
+            }
+
+            // Create and save BBRequirements instance
+            const bbKey = 'some_key_cross_cutting'; 
+            const bbName = 'some_name_cross_cutting'; 
+            const spaceInfo = { version: '1.0.0' }; 
+            const dateOfSave = new Date().toISOString().split('T')[0]; // To match the date format without time
+            const bbRequirement = new BBRequirements({ bbKey, bbName, bbVersion: spaceInfo.version, dateOfSave, requirements: { crossCutting: result.requirements } });
+            await bbRequirement.save();
+            
+            // Assertions for the bbRequirement record
+            expect(bbRequirement).to.have.property('bbKey', bbKey);
+            expect(bbRequirement).to.have.property('bbName', bbName);
+            expect(bbRequirement).to.have.property('bbVersion', spaceInfo.version);
+            expect(bbRequirement).to.have.property('dateOfSave');
+            expect(bbRequirement).to.have.property('requirements').that.is.an('object');
+          
+            // Further checking the structure of the requirements
+            expect(bbRequirement.requirements).to.have.property('crossCutting').that.is.an('array').with.lengthOf(3);
+
+            // Assuming the first requirement should have status REQUIRED
+            expect(bbRequirement.requirements?.crossCutting[0]).to.have.property('status', RequirementStatusEnum.REQUIRED);
+            
+            // Assuming the second requirement should have status RECOMMENDED
+            expect(bbRequirement.requirements?.crossCutting[1]).to.have.property('status', RequirementStatusEnum.RECOMMENDED);
+            
+            // Assuming the third requirement should have status OPTIONAL
+            expect(bbRequirement.requirements?.crossCutting[2]).to.have.property('status', RequirementStatusEnum.OPTIONAL);
+            expect(bbRequirement.requirements).to.have.property('functional').that.is.an('array').with.lengthOf(0);
+          
+          });
+    });
 });
