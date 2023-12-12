402bf06ec515e4e172560d1d31c5fc3573357131
lruzicki <lukasz.ruz@gmail.com>
2023-12-12 14:20:10 +0100

    Rate limiter extended to 1000 request per hour

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -74,7 +74,7 @@ const appConfig: AppConfig = {
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
-  max: 100, 
+  max: 1000, 
   message: "Too many requests from this IP, please try again later."
 });
 
