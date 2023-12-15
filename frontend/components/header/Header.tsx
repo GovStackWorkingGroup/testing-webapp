cb7e36fe42b71fff629b98fc61ce4785469227d3
lruzicki <lukasz.ruz@gmail.com>
2023-12-15 15:20:46 +0100

    Test snapshot update

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
     const token = sessionStorage.getItem('accessToken');
     setIsLoggedIn(!!token);
   });
-  
+
   const handleBackToHomePage = () => {
     router.push('/');
   };
