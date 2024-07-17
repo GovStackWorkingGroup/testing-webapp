080e7e4547fcf7c75215bd993fb01a8abfb07a4f
user <lruzicki>
2024-07-17 11:45:40 +0200

    Revert "release: revert TECH-1049, TECH-1076 because they are not in 'Done' status"
    This reverts commit d230abc56a7f7bc253d7b4827dc026e11109aeb3.

--- frontend/components/Definition.tsx
+++ frontend/components/Definition.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import classNames from 'classnames';
 import { formatTranslationType } from '../service/types';
+import children = ReactMarkdown.propTypes.children;
 
 type DefinitionType = {
   title: formatTranslationType;
   hasRedirecting?: boolean;
-  description: formatTranslationType;
+  description?: formatTranslationType;
   descriptionPartTwo?: formatTranslationType;
   note?: formatTranslationType;
   customStyle?: string;
+  children?: React.ReactNode;
 };
 
 const Definition = ({
@@ -19,6 +21,7 @@ const Definition = ({
   descriptionPartTwo,
   note,
   customStyle,
+  children
 }: DefinitionType) => (
   <div className={classNames('definition-section', customStyle)}>
     <p className="definition-title" data-testid="definition-title">
@@ -27,18 +30,17 @@ const Definition = ({
     <div data-testid="definition-description">
       {hasRedirecting ? (
         <div>
-          {/* @ts-ignore */}
           <ReactMarkdown className="definition-description" linkTarget="_blank">
-            {description || ''}
+            {`${description}`}
           </ReactMarkdown>
-          {/* @ts-ignore */}
           <ReactMarkdown className="definition-description" linkTarget="_blank">
-            {descriptionPartTwo || ''}
+            {`${descriptionPartTwo}`}
           </ReactMarkdown>
         </div>
       ) : (
         <div className="definition-description">{description}</div>
       )}
+      {children}
     </div>
     {note && (
       <p className="definition-note" data-testid="definition-note">
